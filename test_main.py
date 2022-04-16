496e5d6edbf73bc237bb66c3229a778987cdc7e5
LuffyKing <aderinwale17@gmail.com>
2022-04-16 19:41:12 +0100

    update workflows

--- test_main.py
+++ test_main.py
@@ -1,11 +1,11 @@
 from urllib import response
 from fastapi.testclient import TestClient
 
-from .main import app
+from main import app
 
 client = TestClient(app)
 
-def test_read_main():
+def test_root():
     response = client.get("/")
     assert response.status_code ==  200
-    assert response.json() == {"msg": "Hello World"}
+    assert response.json() == {"message": "Hello World"}
